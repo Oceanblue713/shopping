a5aa428b587c9639899584c7307c32678b5d0636
Anri Liy <amedamachip713@gmail.com>
2021-01-21 19:35:34 -0800

    Style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+
 
 const Navbar = () => {
   return (
-    <div>
-      <h3>Nav</h3>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark px-sm-5 bg-primary">
+      <Link to='/'>
+        <img src={logo} alt="store" className="navbar-brand" />
+      </Link>
+      <ul className="navbar-nav align-iteems-center">
+        <li className="nav-item ml-5">
+          <Link to="/" className="nav-link">Products</Link>
+        </li>
+      </ul>
+      <Link to="/cart" className="ml-auto">
+        <button><i className="fas fa-cart-plus" />My Cart</button>
+      </Link>
+    </nav>
   );
 }
 
