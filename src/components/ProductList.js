43629c8de7fbcbe8f9243d24e90daa384d40aa29
Anri Liy <amedamachip713@gmail.com>
2021-02-01 18:13:49 -0800

    Product List

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Product from './Product';
-
+import Title from './Title';
+import { storeProducts } from '../data';
 const ProductList = () => {
+
+  const [products, setProducts] = useState([storeProducts]);
+
+  console.log(storeProducts);
+
   return (
-    <div>
-      <h3>Product List</h3>
-      <Product />
-    </div>
+    <React.Fragment>
+      <div className="py-5">
+        <div className="container">
+          <div className="row">
+            <Title name="our" title="products" />
+
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
