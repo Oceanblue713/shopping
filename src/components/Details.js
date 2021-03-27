06580aaa4e492b7f33763ac32c0f4c35359bbcc3
Anri Liy <amedamachip713@gmail.com>
2021-03-27 13:15:40 -0700

    Product details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import Product from './Navbar';
+import {ProductConsumer} from '../context';
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
 
 const Details= () => {
   return (
-    <div>
-      <h3>Details</h3>
-    </div>
+    <ProductConsumer>
+      {value => {
+        const {id, company, img, info, price, title, inCart} = value.detail;
+        return (
+          <div className="container">
+
+          </div>
+        )
+      }}
+    </ProductConsumer>
   );
 }
 
